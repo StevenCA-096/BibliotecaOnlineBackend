1ec574c8d093a9d14e07e0ca534294b69f05c572
KLG <kucsen2003@gmail.com>
2024-05-23 08:58:58 -0600

    TypeORMModule Sql12

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    BooksModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
